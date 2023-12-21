68f7bdd4dd29ab7a37eef10215cfb08bac583c20
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2023-12-21 09:13:42 +0100

    maj data augm

--- Code/data_augm.py
+++ Code/data_augm.py
@@ -12,19 +12,27 @@ import Augmentor
 import os
 
 
-SRC_DIR = os.path.abspath('images/images_final')
+SRC_DIR = os.path.abspath('Code/images/images_final/')
 
 echantillon = 400
+for directory in os.listdir(SRC_DIR):
+    directory_path = os.path.join(SRC_DIR, directory)
 
-p = Augmentor.Pipeline(SRC_DIR)
+    p = Augmentor.Pipeline(directory_path)
 
-p.flip_top_bottom(probability=0.8)
+    p.flip_top_bottom(probability=0.8)
 
-p.random_brightness(probability=0.3,min_factor=0.3,max_factor=1.2)
+    p.random_brightness(probability=0.3,min_factor=0.3,max_factor=1.2)
 
-p.random_distortion(probability=0.6,grid_width=4,grid_height=4,magnitude=8)
+    p.random_distortion(probability=0.6,grid_width=4,grid_height=4,magnitude=8)
 
-p.sample(echantillon)
+    p.sample(echantillon)
 
+    OUT_DIR = os.path.join(directory_path, 'output')
+    if os.path.isdir(OUT_DIR):
+        for filename in os.listdir(OUT_DIR):
+            SRC = os.path.join(OUT_DIR, filename)
+            OUT = os.path.join(directory_path, filename)
+            os.rename(SRC, OUT)
+        os.rmdir(OUT_DIR)
 
-## On copiera le dossier output généré dans le répertoire d'images_final
